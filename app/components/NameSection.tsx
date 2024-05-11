c4ed11cd8c5e1fa05c812382c5a8ea6542fee225
Matthew Arenivas <148577119+matheuarenivas@users.noreply.github.com>
2024-05-10 21:01:01 -0700

    Update NameSection.tsx

--- app/components/NameSection.tsx
+++ app/components/NameSection.tsx
@@ -6,24 +6,24 @@ import { TypeAnimation } from 'react-type-animation';
 class NameSection extends Component {
     render() {
         return (
-            <div className="lg:mt-0">
-                <h1 className='text-white text-2xl sm:text-5xl lg:text-6xl font-extrabold'> 
+            <div className=" mt-4 lg:mt-0"> {/* Center alignment and vertical spacing */}
+                <h1 className='text-white text-2xl sm:text-5xl lg:text-6xl font-extrabold'>
                     <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-red-300 to-pink-400'>
-                    {`Hello, I'm `} 
+                        Hello, I'm
                     </span>
-                    <br className="hidden lg:block" />
+                </h1>
+                <div className='text-white text-2xl sm:text-5xl lg:text-6xl font-extrabold' > {/* New line for TypeAnimation */}
                     <TypeAnimation
                         sequence={[
-                            'Matthew Arenivas',
-                            1000,
-                            'a Software Engineer ',
-                            1000
+                            'Matthew Arenivas', 1000,
+                            'a Software Engineer', 1000
                         ]}
-                        wrapper="span"
+                        wrapper="h1" // Use the same style as the line above
+                        cursor={true}
                         speed={3}
                         repeat={Infinity}
                     />
-                </h1>
+                </div>
             </div>
         );
     }
